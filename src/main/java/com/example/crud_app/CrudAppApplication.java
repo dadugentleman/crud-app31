e877afe506d3de2df2b8377ba601961ac4b72331
Sandu Guzun <sanduguzun762@gmail.com>
2025-05-31 15:25:19 +0300

    Initial commit

--- src/main/java/com/example/crud_app/CrudAppApplication.java
+++ src/main/java/com/example/crud_app/CrudAppApplication.java
@@ -1,7 +1,10 @@
 package com.example.crud_app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudAppApplication {
@@ -9,5 +12,12 @@ public class CrudAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrudAppApplication.class, args);
 	}
+	
 
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner ->{
+			System.out.println("Hello World");
+		};
+	}
 }
